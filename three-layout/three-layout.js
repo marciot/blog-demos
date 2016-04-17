31634b3bed90f3329929c501794e0abd3a2f63c9
Marcio T <mlt4356-github@yahoo.com>
2016-04-17 11:07:14 -0600

    Added ability to zoom in plus code cleanup.

--- three-layout/three-layout.js
+++ three-layout/three-layout.js
@@ -11,40 +11,39 @@ var motionEnabled = true;
   
 function init() {
   
-  	/* A tutorial on how to properly handling sizing for a THREE.js
-  	 * that renders INSIDE a canvas element (rather than the window)
-  	 * is here:
-  	 *
-  	 *    http://www.rioki.org/2015/04/19/threejs-resize-and-canvas.html
-  	 *
-  	 */
-  	 
-  	var canvas = document.getElementById("webgl");
-	
-    scene  = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera( 70, canvas.clientWidth / canvas.clientHeight, 1, 1000);
+	/* A tutorial on how to properly handling sizing for a THREE.js
+	 * that renders INSIDE a canvas element (rather than the window)
+	 * is here:
+	 *
+	 *    http://www.rioki.org/2015/04/19/threejs-resize-and-canvas.html
+	 *
+	 */
+	 
+	var canvas = document.getElementById("webgl");
+	camera = new THREE.PerspectiveCamera( 70, canvas.clientWidth / canvas.clientHeight, 1, 1000);
   
+	scene  = new THREE.Scene();
 	var light = new THREE.AmbientLight( 0x555555 );
-    scene.add(light);
+	scene.add(light);
 	
-    var light = new THREE.DirectionalLight( 0xffffff );
-    light.position.set( 0, 1, 1 ).normalize();
-    scene.add(light);
+	var light = new THREE.DirectionalLight( 0xffffff );
+	light.position.set( 0, 1, 1 ).normalize();
+	scene.add(light);
   
-    var geometry = new THREE.CubeGeometry(10, 10, 10);
-    var material = new THREE.MeshLambertMaterial( { color: 0x9999ff } );
+	var geometry = new THREE.CubeGeometry(10, 10, 10);
+	var material = new THREE.MeshLambertMaterial( { color: 0x9999ff } );
   
-    mesh = new THREE.Mesh(geometry, material);
-    mesh.position.z = -20;
-    scene.add( mesh );
+	mesh = new THREE.Mesh(geometry, material);
+	mesh.position.z = -20;
+	scene.add( mesh );
   
 	/* Setting alpha to true allows the canvas to be styled with CSS */
-    renderer = new THREE.WebGLRenderer({alpha: true, canvas: canvas});
-    canvas.width  = canvas.clientWidth;
+	renderer = new THREE.WebGLRenderer({alpha: true, canvas: canvas});
+	canvas.width  = canvas.clientWidth;
 	canvas.height = canvas.clientHeight;
 	renderer.setViewport(0, 0, canvas.clientWidth, canvas.clientHeight);
 	
-    window.addEventListener( 'resize', onWindowResize, false );
+	window.addEventListener( 'resize', onWindowResize, false );
 }
 
 function animate() {
@@ -54,11 +53,11 @@ function animate() {
   
 		render();
 	}
-    requestAnimationFrame( animate );
+	requestAnimationFrame( animate );
 }
   
 function render() {
-    renderer.render( scene, camera );
+	renderer.render( scene, camera );
 }
 
 function toggleRotation() {
@@ -74,9 +73,9 @@ function onWindowResize() {
 	/* The resizing code here also comes from rioki */
 	var canvas = document.getElementById('webgl');
 	canvas.width  = canvas.clientWidth;
-  	canvas.height = canvas.clientHeight;
-  	renderer.setViewport(0, 0, canvas.clientWidth, canvas.clientHeight);
-  	camera.aspect = canvas.clientWidth / canvas.clientHeight;
-  	camera.updateProjectionMatrix();
-    render();
+	canvas.height = canvas.clientHeight;
+	renderer.setViewport(0, 0, canvas.clientWidth, canvas.clientHeight);
+	camera.aspect = canvas.clientWidth / canvas.clientHeight;
+	camera.updateProjectionMatrix();
+	render();
 }
